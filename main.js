45b76a2b8ea8b01d2924cce1931e30a99e7ab26d
jbeardshear <jakebeardshear@gmail.com>
2017-03-14 15:49:39 -0700

    Restructured site layout/idea.

---
+++ main.js
@@ -0,0 +1,105 @@
+var items = [
+  {
+    brand: 'Leatt',
+    model: '2017 GPX 6.5 Helmet',
+    price: '$599'
+  },
+  {
+    brand: 'Fox Racing',
+    model: 'V3 Helmet - A1 Moth LE',
+    price: '$459'
+  },
+  {
+    brand: 'Troy Lee Designs',
+    model: 'SE4 Composite Helmet - Freedom',
+    price: '$450'
+  },
+  {
+    brand: 'Bell',
+    model: 'Moto-9 Carbon Flex Helmet - Day in the Dirt',
+    price: '$699'
+  },
+  {
+    brand: '6D Helmets',
+    model: 'ATR-1 Helmet - Camo',
+    price: '$550'
+  },
+  {
+    brand: 'Just1',
+    model: 'J12 Carbon Helmet - Aster Italy',
+    price: '$320'
+  }
+]
+
+
+// <div class="container-fluid">
+//   <div class="page-header">
+//     <h1 id="logo">Brain Buckets <small>Protect your noggin!</small></h1>
+//   </div>
+//   <div class="row">
+//     <div class="col-xs-6" id="helmets">
+//       <div class="thumbnail">
+//         <img src="leatt helmet.jpg" alt="helmets">
+//         <div class="caption">
+//           <h2>Leatt</h2>
+//           <p>2017 GPX 6.5 Helmet</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col-xs-6" id="boots">
+//       <div class="thumbnail">
+//         <img src="fox helmet.jpg" alt="boots">
+//         <div class="caption">
+//           <h2>Fox Racing</h2>
+//           <p>V3 Helmet - A1 Moth LE</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="row">
+//     <div class="col-xs-6" id="jerseys">
+//       <div class="thumbnail">
+//         <img src="tld helmet.jpg" alt="jerseys">
+//         <div class="caption">
+//           <h2>Troy Lee Designs</h2>
+//           <p>SE4 Composite Helmet - Freedom</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col-xs-6" id="pants">
+//       <div class="thumbnail">
+//         <img src="bell helmet.jpg" alt="pants">
+//         <div class="caption">
+//           <h2>Bell</h2>
+//           <p>Moto-9 Carbon Flex Helmet - Day in the Dirt</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="row">
+//     <div class="col-xs-6" id="goggles">
+//       <div class="thumbnail">
+//         <img src="6d helmet.jpg" alt="goggles">
+//         <div class="caption">
+//           <h2>6D Helmets</h2>
+//           <p>ATR-1 Helmet - Camo</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//     <div class="col-xs-6" id="gloves">
+//       <div class="thumbnail">
+//         <img src="just1 helmet.jpg" alt="gloves">
+//         <div class="caption">
+//           <h2>Just1</h2>
+//           <p>J12 Carbon Helmet - Aster Italy</p>
+//           <p><a href="#" class="btn btn-primary" role="button">See More</a></p>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// </div>
