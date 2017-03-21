b5f0da52ea1feb8edf564e019bd1eca67de4ef41
jbeardshear <jakebeardshear@gmail.com>
2017-03-21 14:19:17 -0700

    Further styling to details page. #3

---
+++ data.js
@@ -0,0 +1,38 @@
+var items = [
+  {
+    brand: 'Leatt',
+    model: 'GPX 6.5 Helmet',
+    price: '$599',
+    picture: 'pictures/leatthelmet.jpg'
+  },
+  {
+    brand: 'Fox Racing',
+    model: 'V3 Helmet - A1 Moth LE',
+    price: '$459',
+    picture: 'pictures/foxhelmet.jpg'
+  },
+  {
+    brand: 'Troy Lee Designs',
+    model: 'SE4 Composite Helmet - Freedom',
+    price: '$450',
+    picture: 'pictures/tldhelmet.jpg'
+  },
+  {
+    brand: 'Bell',
+    model: 'Moto-9 Carbon Flex Helmet - Day in the Dirt',
+    price: '$699',
+    picture: 'pictures/bellhelmet.jpg'
+  },
+  {
+    brand: '6D Helmets',
+    model: 'ATR-1 Helmet - Camo',
+    price: '$550',
+    picture: 'pictures/6Dhelmet.jpg'
+  },
+  {
+    brand: 'Just1',
+    model: 'J12 Carbon Helmet - Aster Italy',
+    price: '$320',
+    picture: 'pictures/just1helmet.jpg'
+  }
+]
